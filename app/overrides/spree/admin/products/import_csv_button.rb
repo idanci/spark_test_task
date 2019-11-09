2bb9985c14dc607cca0a432a5c5ba2c393c47c1d
Ivan Danci <bah4uk@gmail.com>
2019-11-10 00:31:09 +0200

    Remove redundant partial

--- app/overrides/spree/admin/products/import_csv_button.rb
+++ app/overrides/spree/admin/products/import_csv_button.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/admin/products/index',
                      name: 'import_csv_button',
                      insert_before: "erb[silent]:contains('if can?(:create, Spree::Product)')",
-                     partial: 'spree/admin/products/import_csv_button')
+                     :text => "<%= button_link_to 'Import CSV', admin_products_import_path %>")
